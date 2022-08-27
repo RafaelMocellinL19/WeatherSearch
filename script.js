a5f93f054ffc2950d8a2dc4b7098755ee2c63255
Rafael Mocellin <rafael.mocellin.leszczynski@gmail.com>
2022-08-27 19:03:01 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,87 @@
+document.querySelector('.busca').addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    let input = document.querySelector('#searchInput').value;
+    
+    if(input !== '') {
+        clearInfo();
+        showWarning('Carregando...');
+
+        let results = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=
+        ${encodeURI(input)}&units=metric&lang=pt_br&appid=d06cdb298fafc83c520d5ab677fc477e`);
+        let json = await results.json();
+
+        if(json.cod === 200) {
+            showInfo({
+                name: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windAngle: json.wind.deg
+            });
+        } else {
+            clearInfo();
+            showWarning('Não encontramos esta localização.');
+        }
+    } else {
+        clearInfo();
+    }
+});
+
+function showInfo(obj) {
+    showWarning('');
+
+    document.querySelector('.titulo').innerHTML = `${obj.name}, ${obj.country}`;
+    
+    document.querySelector('.tempInfo').innerHTML = `${obj.temp} <sup>ºC</sup>`;
+    document.querySelector('.ventoInfo').innerHTML = `${obj.windSpeed} <span>km/h</span>`;
+
+    document.querySelector('.temp img').setAttribute('src', 
+    `http://openweathermap.org/img/wn/${obj.tempIcon}@2x.png`);
+    
+    document.querySelector('.ventoPonto').style.transform = `rotate(${obj.windAngle-90}deg)`;
+
+    document.querySelector('.resultado').style.display = 'block';
+}
+
+function clearInfo() {
+    showWarning('');
+    document.querySelector('.resultado').style.display = 'none';
+}
+
+function showWarning(msg) {
+    document.querySelector('.aviso').innerHTML = msg;
+}
+
+
+let digitalElement = document.querySelector('.digital');
+let sElement = document.querySelector('.p_s');
+let mElement = document.querySelector('.p_m');
+let hElement = document.querySelector('.p_h');
+
+function updateClock() {
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+    let day = now.getFullYear();
+
+    digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`;
+
+    let sDeg = ((360 / 60) * second) - 90;
+    let mDeg = ((360 / 60) * minute) - 90;
+    let hDeg = ((360 / 12) * hour) - 90;
+ 
+
+    
+}
+
+function fixZero(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
+
+
